51f0f671b6ef337210f5d093502fdbf877190273
lw <huck34@163.com>
2019-08-26 10:56:46 +0800

    wsddd

---
+++ test68/src/main/java/test68/test68/App.java
@@ -0,0 +1,13 @@
+package test68.test68;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
